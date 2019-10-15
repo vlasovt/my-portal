e5f758bd27d1313791e72c06bcde7dd75a1739fd
vlasovt <tarasvlasov@gmail.com>
2019-10-14 20:44:34 -0400

    Updates

---
+++ precache-manifest.5c699d33afc97f3d6d4ac0d7f6fcfe7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c007100c36f07164cfd1776828d247f6",
+    "url": "%20//vlasovt.github.io/my-portal/index.html"
+  },
+  {
+    "revision": "68f1be6e4835699240f9",
+    "url": "%20//vlasovt.github.io/my-portal/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "aac62aa390a7fd2d235c",
+    "url": "%20//vlasovt.github.io/my-portal/static/js/2.ca6a5a52.chunk.js"
+  },
+  {
+    "revision": "68f1be6e4835699240f9",
+    "url": "%20//vlasovt.github.io/my-portal/static/js/main.7f10ea05.chunk.js"
+  },
+  {
+    "revision": "fad66510da722322e7e6",
+    "url": "%20//vlasovt.github.io/my-portal/static/js/runtime-main.1849eac7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "%20//vlasovt.github.io/my-portal/static/media/logo.25bf045c.svg"
+  }
+]);
